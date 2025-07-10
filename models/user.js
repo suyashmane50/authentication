c411663693c8a17c6a569db764f8ebfaaab58733
Suyash Mane <suyashmane584@gmail.com>
2025-07-10 12:55:50 +0530

    changes..

--- models/user.js
+++ models/user.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect((process.env.MONGO_URI), {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("Connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
